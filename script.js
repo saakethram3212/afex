4edeb807de77d52cade3eba5f50b60c2d9546a9d
Saakethhhh <saakethram00@gmail.com>
2026-03-04 16:29:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+},{threshold:0.2});
+
+document.querySelectorAll(".video-card, .about-box").forEach(el=>{
+    observer.observe(el);
+});
